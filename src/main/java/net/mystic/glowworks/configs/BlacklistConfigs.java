660b1a48e7b18b0efbb3a39db2e5eae3ec172ef8
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-11-08 22:41:29 -0700

    BlacklistConfigs: Added GLASS_BLACKLIST

--- src/main/java/net/mystic/glowworks/configs/BlacklistConfigs.java
+++ src/main/java/net/mystic/glowworks/configs/BlacklistConfigs.java
@@ -10,7 +10,8 @@ import java.util.function.Supplier;
 
 public class BlacklistConfigs {
 
-    public static Supplier<List<String>> BLACKLIST;
+    public static Supplier<List<String>> LIGHT_BLACKLIST;
+    public static Supplier<List<String>> GLASS_BLACKLIST;
 
     public static ConfigSpec SPEC;
     private static boolean wasInit = false;
@@ -23,7 +24,7 @@ public class BlacklistConfigs {
 
         String commentBuilder = """
                 Blacklist blocks from being applied with emitters
-                    EXAMPLE: blacklist = [
+                    EXAMPLE: light_blacklist = [
                         "minecraft:torch",\t\t\tCOMMENT: a simple block_id for a torch. It's blacklisted
                         "minecraft:.*torch",\t\tCOMMENT: this is a regex, soul_torch, soul_wall_torch, torch, wall_torch from Minecraft's are blacklisted.
                         "minecraft:.*",\t\t\t\tCOMMENT: This is a regex, all of blocks from Minecraft's are blacklisted.
@@ -33,9 +34,15 @@ public class BlacklistConfigs {
 
         builder.comment(commentBuilder);
 
-        builder.push("blacklist");
+        builder.push("light_blacklist");
 
-        BLACKLIST = builder.define("blacklist", List.of());
+        LIGHT_BLACKLIST = builder.define("light_blacklist", List.of());
+
+        builder.pop();
+
+        builder.push("glass_blacklist");
+
+        GLASS_BLACKLIST = builder.define("glass_blacklist", List.of());
 
         builder.pop();
 
